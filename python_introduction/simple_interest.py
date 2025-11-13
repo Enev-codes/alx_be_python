2382d7f5c0a4840281be4aafbb708a379b06694f
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-11-13 15:24:28 +0100

    Fixed bugs

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,3 @@
-def find_interest(p, r, t):
-    return p * r * t
-
 def render_interest(interest):
     print("The simple interest is : {}".format(interest))
 
@@ -8,7 +5,7 @@ def main():
     principal = 1000
     rate = 0.05
     time = 3
-    interest = find_interest(principal, rate, time)
+    interest = principal * rate * time
     render_interest(interest)
 
 if __name__ == '__main__':
