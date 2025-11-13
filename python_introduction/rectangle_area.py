534e12ec49200603974cbd78ca29c3b539faf01e
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-11-13 16:11:25 +0100

    Fixed bugs

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,13 +1,10 @@
-def calc_area(l, w):
-    return l * w
-
 def render_area(area):
     print("The area of the rectangle is: {}".format(area))
 
 def main():
     length = 10
     width = 5
-    area = calc_area(length, width)
+    area = length * width
     render_area(area)
 
 
