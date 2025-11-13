37d81c3f41a5e3ae6e43a75a04078c2ea76ccf8c
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-11-13 16:35:09 +0100

    debugging Debugger

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,6 @@
 def convert():
     hours = 2
-    seconds = hours * 60 * 60
+    seconds = hours * 3600
     print("{}(s) hours is {} seconds.".format(hours, seconds))
 
 if __name__ == '__main__':
