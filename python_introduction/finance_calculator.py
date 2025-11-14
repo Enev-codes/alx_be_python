de79c9439b7f5040c69702b893738dc74d93791e
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-11-14 06:15:12 +0100

    Debugging Debugger

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 def main():
     # Prompt user for data
-    m_income = float(input("Enter your monthly income: "))
-    m_expens = float(input("Enter your total monthly expenses: "))
+    monthly_income = float(input("Enter your monthly income: "))
+    monthly_expenses = float(input("Enter your total monthly expenses: "))
 
     # Calculate monthly savings
     m_savings = m_income - m_expens
@@ -11,7 +11,7 @@ def main():
     time = 1
     interest = m_savings * (time * 12) * rate
     pa_savings = m_savings * (time * 12) + interest # pas
-    print("Your monthly savings are ${}.\nProjected savings after one year, with interest, is: ${}.".format(int(m_savings), int(pa_savings)))
+    print("Your monthly savings are ${}.\nProjected savings after one year, with interest, is: ${}.".format(m_savings, pa_savings))
 
 
 if __name__ == '__main__':
