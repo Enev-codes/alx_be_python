f245b5202ad23dfbe6e14027cc0210b4aeb0739d
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-11-14 06:18:37 +0100

    Debugging Debugger

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,14 +4,14 @@ def main():
     monthly_expenses = float(input("Enter your total monthly expenses: "))
 
     # Calculate monthly savings
-    m_savings = m_income - m_expens
+    monthly_savings = monthly_income - monthly_expenses
 
     # Project annual savings
     rate = 0.05
     time = 1
     interest = m_savings * (time * 12) * rate
     pa_savings = m_savings * (time * 12) + interest # pas
-    print("Your monthly savings are ${}.\nProjected savings after one year, with interest, is: ${}.".format(m_savings, pa_savings))
+    print("Your monthly savings are ${}.\nProjected savings after one year, with interest, is: ${}.".format(float(m_savings), float(pa_savings)))
 
 
 if __name__ == '__main__':
