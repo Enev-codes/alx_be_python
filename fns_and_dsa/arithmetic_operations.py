4f38490fca54d35273e7b74d67aed0781b2967ca
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-11-16 06:54:13 +0100

    Added divide by zero case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,22 +11,23 @@ def div(num1, num2):
     return num1 / num2
 
 def perform_operation(num1, num2, operation):
-    if operation == 'divide' and num2 == 0:
-        return None
-
-    oper_map = {
-            'add': add,
-            'subtract': sub,
-            'multiply': mul,
-            'divide': div
-            }
-    try:
-        func = oper_map.get(operation)(num1, num2)
-        if func:
-            return func
-        return None
-    except:
-        print("[ Error ]: Invalid operation")
+    if num2 == 0:
+        if operation == 'divide':
+            return None
+    elif num2:
+        oper_map = {
+                'add': add,
+                'subtract': sub,
+                'multiply': mul,
+                'divide': div
+                }
+        try:
+            func = oper_map.get(operation)(num1, num2)
+            if func:
+                return func
+            return None
+        except:
+            print("[ Error ]: Invalid operation")
 
 
 if __name__ == '__main__':
