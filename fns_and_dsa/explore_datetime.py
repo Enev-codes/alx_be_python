8ea857461e96abcfa58fd3f0d8307a1c1dcd5448
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-11-17 20:39:29 +0100

    Separated concerns into separate functions

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,21 +1,24 @@
 from datetime import datetime, timedelta
 
 
-def display_current_datetime(future):
+def display_current_datetime(present):
+    print("Current date and time: ", present)
+
+def display_future_datetime(future):
     print("Future date: ", future)
 
-def forward_time(present, delta):
+def calculate_future_date(present, delta):
     return (present + timedelta(days=delta))
 
 
 def main():
     current_date = datetime.now()
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
-    print("Current date and time: ", formatted_date)
+    display_current_datetime(formatted_date)
     days = int(input("Enter the number of days to add to the current date: "))
-    future = forward_time(current_date, days)
+    future = calculate_future_date(current_date, days)
     formatted_future = future.strftime("%Y-%m-%d")
-    display_current_datetime(formatted_future)
+    display_future_datetime(formatted_future)
 
 if __name__ == '__main__':
     main()
