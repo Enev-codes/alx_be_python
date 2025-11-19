8d9f4b62c8225109d89acd2d414c6e220b92158d
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-11-19 03:37:31 +0100

    Fixed the mathematical errors in functions

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,12 +2,12 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    convert = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
-    print(f"{fahrenheit}°F is {convert}°C")
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(f"{fahrenheit}°F is {celsius}°C")
 
 def convert_to_fahrenheit(celsius):
-    convert = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
-    print(f"{celsius}°C is {convert}°F")
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    print(f"{celsius}°C is {fahrenheit}°F")
 
 def main():
     try:
